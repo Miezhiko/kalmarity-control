bb41889cc8dfc8e81c2467d65c6580f95d2f5db3
Miezhiko <Miezhiko@gmail.com>
2023-08-08 11:59:05 +0400

    cleanup + put readline on ctrlc tokio loop
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,51 @@
 use env_logger::Env;
 
-use std::io;
-use std::time::Duration;
+use std::{
+  io,
+  time::Duration
+};
 
-use log::{ info, error };
+use log::error;
 
-use rdkafka::config::ClientConfig;
-use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::{
+  config::ClientConfig,
+  producer::{FutureProducer, FutureRecord}
+};
 
 use clap::Parser;
 
-/// Kalmarity control util
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  /// command to execute (unused)
   #[arg(long)]
   command: Option<String>,
 
-  /// channel id
   #[arg(short, long, default_value_t = 0)]
   channel: u64,
 
-  /// user id to mention
   #[arg(short, long, default_value_t = 0)]
   user: u64,
 
-  /// msg id to reply
   #[arg(short, long, default_value_t = 0)]
   msg: u64
 }
 
-async fn produce(args: Args, text: &str) {
+async fn produce(args: &Args, text: &str) {
   let producer: &FutureProducer = &ClientConfig::new()
     .set("bootstrap.servers", "localhost:9092")
     .set("message.timeout.ms", "5000")
     .create()
     .expect("Producer creation error");
-
-  let future = async move {
-    let k_key = format!("{}|{}|{}"
-                       , args.channel
-                       , args.user
-                       , args.msg);
-    let delivery_status = producer
-      .send(
-        FutureRecord::to("Kalmarity")
-          .payload(text)
-          .key(&k_key),
-        Duration::from_secs(0),
-      )
-      .await;
-
-    // This will be executed when the result is received.
-    info!("Delivery status for message received");
-    delivery_status
-  };
-
-  let _ = future.await;
+  let _delivery_status = producer
+    .send(
+      FutureRecord::to("Kalmarity")
+        .payload(text)
+        .key( &format!("{}|{}|{}"
+            , args.channel
+            , args.user
+            , args.msg)),
+      Duration::from_secs(0),
+    ).await;
 }
 
 #[tokio::main(worker_threads=8)]
@@ -70,10 +58,17 @@ async fn main() -> anyhow::Result<()> {
   let args = Args::parse();
 
   let mut input = String::new();
-  match io::stdin().read_line(&mut input) {
-    Ok(_)       => produce(args, input.trim()).await,
-    Err(error)  => error!("Error: {}", error)
-  }
+
+  tokio::spawn( async move {
+    loop {
+      match io::stdin().read_line(&mut input) {
+        Ok(_)       => produce(&args, input.trim()).await,
+        Err(error)  => error!("Error: {}", error)
+      }
+    }
+  }).await?;
+
+  tokio::signal::ctrl_c().await?;
 
   Ok(())
 }
